35ee2568a882512983a03698c060ab827b5406b4
DoomLuchin <66922771+DoomLuchin@users.noreply.github.com>
2023-11-12 20:03:12 -0500

    add TestMain

---
+++ TestsMain.py
@@ -0,0 +1,32 @@
+from modelo.examen import Examen
+from  modelo.paciente import Paciente
+from main import validar_cita_paciente
+from datetime import datetime
+import unittest
+
+class TestMain(unittest.TestCase):  
+    DIAS_ATENCION = [0,1,2,3,4]
+    HORARIO_APERTURA = datetime.strptime("7:00", "%H:%M").time()
+    HORARIO_CIERRE = datetime.strptime("16:00", "%H:%M").time()
+    FERIADOS = {"08-10", "11-03"} 
+    FECHA_HORA_EXAMEN = datetime(2023, 11, 10, 9, 0)
+
+    def test_validar_cita_paciente(self):  
+        paciente = Paciente
+        paciente.identificacion = "1721872073"
+
+        examenes = []
+        examen = Examen
+        examen.paciente = Paciente
+        examen.paciente.identificacion = "1721872073"
+        examen.fecha_hora_examen = datetime(2023, 11, 10, 9, 0)
+        examenes.append(examen)
+
+        with self.assertRaises(ValueError) as contexto:
+            validar_cita_paciente(paciente, self.FECHA_HORA_EXAMEN, examenes)
+        self.assertEqual(str(contexto.exception), "El paciente ya tiene una cita en el mismo horario.")
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
