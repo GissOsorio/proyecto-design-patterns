2d5c616775a49dbaa62e90047584fd12786c2845
DoomLuchin <66922771+DoomLuchin@users.noreply.github.com>
2023-11-12 19:29:40 -0500

    Update TestPersona

--- TestPersona.py
+++ TestPersona.py
@@ -1,14 +1,21 @@
 from modelo.persona import Persona
 from modelo.paciente import Paciente
+from datetime import datetime
 import unittest
 
-class TestPaciente(unittest.TestCase):  
+class TestPersona(unittest.TestCase):  
+
+    def test_es_mayor_de_edad(self):  
+        
+        persona = Persona
+        persona.fecha_nacimiento = datetime(1988, 11, 10, 9, 0)
+        es_mayor_de_edad = persona.es_mayor_de_edad(persona)
+        self.assertEqual(es_mayor_de_edad, True)         
 
     def test_tiene_apoderado(self):  
         
         persona = Persona
         persona.apoderado = Persona
-        persona.es_mayor_de_edad
 
         paciente = Paciente
         tiene_apoderado = paciente.tiene_apoderado(persona)
